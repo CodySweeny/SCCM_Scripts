ef3768d45fcc2788bf306c9d06a21f9e5c9237da
Cody Sweeny <73262426+CodySweeny@users.noreply.github.com>
2020-10-21 20:24:36 -0500

    Remove files after 90 days

---
+++ RemoveFilesAfter.ps1
@@ -0,0 +1,10 @@
+﻿#Cody Sweeny
+#https://www.thomasmaurer.ch/2010/12/powershell-delete-files-older-than/
+#9/15/2020
+$Path = "C:\*.hprof"
+$Path1 = "C:\*.mdmp"
+$old = 90
+$now = Get-Date
+
+Get-ChildItem -path $Path |  Where { ((Get-Date)-$_.LastWriteTime).days -gt $Old | Remove-Item
+Get-ChildItem -path $Path1 | Where { ((Get-Date)-$_.LastWriteTime).days -gt $Old | Remove-Item
