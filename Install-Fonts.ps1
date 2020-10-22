e349284e63d5cac6221644285e6f0b995bf0137a
Cody Sweeny <73262426+CodySweeny@users.noreply.github.com>
2020-10-21 20:14:10 -0500

    Manual Font Installation from Directory

---
+++ Install-Fonts.ps1
@@ -0,0 +1,36 @@
+﻿#Cody Sweeny
+#Based on:https://www.powershellgallery.com/packages/trbdk3-Install-Fonts/1.0/Content/trbdk3-Install-Fonts.ps1
+#With parameters removed - Great for SCCM, Used with the App Deploy  Toolkit
+#10/21/2020
+
+    if (Test-Path -Path $dirFiles) {
+        $FontItem = Get-Item -Path $FontPath
+        if ($FontItem -is [IO.DirectoryInfo]) {
+            if ($Recurse) {
+                $Fonts = Get-ChildItem -Path $FontItem -Include ('*.fon','*.otf','*.ttc','*.ttf') -Recurse
+            } else {
+                $Fonts = Get-ChildItem -Path "$FontItem\*" -Include ('*.fon','*.otf','*.ttc','*.ttf')
+            }
+
+            if (!$Fonts) {
+                throw ('Unable to locate any fonts in provided directory: {0}' -f $FontItem.FullName)
+            }
+        } elseif ($FontItem -is [IO.FileInfo]) {
+            if ($FontItem.Extension -notin ('.fon','.otf','.ttc','.ttf')) {
+                throw ('Provided file does not appear to be a valid font: {0}' -f $FontItem.FullName)
+            }
+
+            $Fonts = $FontItem
+        } else {
+            throw ('Expected directory or file but received: {0}' -f $FontItem.GetType().Name)
+        }
+    } else {
+        throw ('Provided font path does not appear to be valid: {0}' -f $dirFiles)
+    }
+
+    foreach ($Font in $Fonts) {
+        Write-Host ('Installing font: {0}' -f $Font.BaseName)
+        #$Font
+        Copy $Font "C:\Windows\Fonts"
+        New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name         
+    }
