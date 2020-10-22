b98258825a392402521edec1af8600c514d1b772
Cody Sweeny <73262426+CodySweeny@users.noreply.github.com>
2020-10-21 20:18:03 -0500

    Automatic Hyper-V VM Creation
    Creates a prompt that allows you to delete the virtual machine. I'm always spinning up VMs for testing task sequences.

---
+++ VMCreation.ps1
@@ -0,0 +1,24 @@
+﻿$VMName=Read-Host "Please enter a hostname for your new VM"
+$HDD= 40GB #HDD Size
+$MEM= 4096MB #RAM
+$HVHost= "" #Hyper-V Host
+
+Invoke-Command -ComputerName $HVHost -ArgumentList $VMName,$HDD,$MEM -ScriptBlock {
+param($VMName,$HDD,$MEM)
+New-VM -name $VMName -MemoryStartupBytes $MEM -Generation 2 -Path V:\$VMName\ -NewVHDPath V:\$VMName\VirtualHardDrive\HDD.vhdx -NewVHDSizeBytes $HDD -SwitchName "New Virtual Switch"
+Start-VM -name $VMName
+}
+& "C:\windows\System32\vmconnect.exe" $HVHost $VMName
+$a = new-object -comobject wscript.shell 
+$intAnswer = $a.popup("Delete $VMName", ` 
+0,"Delete VM?",4) 
+If ($intAnswer -eq 6) { 
+    Invoke-Command -ComputerName $HVHost -ArgumentList $VMName -ScriptBlock {
+    param($VMName)
+    Stop-VM -Name $VMName -TurnOff
+    Remove-VM -Name $VMName -Force 
+    Remove-Item -Path V:\$VMName\ -Recurse
+}    
+} else { 
+    $a.popup("THE VM LIVES ON") 
+} 
